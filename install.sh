f515d2f896028c8ea580611d2f7d9459e3fe6147
Jamie Morgan-Ward <jamie@blacksheepdesign.co.nz>
2018-02-14 12:03:33 +1300

    Added initial version of install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create temporary directory with install files.
+BINTEMP=`mktemp -d 2>/dev/null || mktemp -d -t 'bsd-install'`
+cd $BINTEMP
+
+# Copy repository and extract into tmp directory
+curl -sL https://github.com/blacksheepdesign/bsd-scripts/archive/master.tar.gz | tar xz
+
+# Copy the scripts to /usr/local
+rsync -a $BINTEMP/ /usr/local/bsd-scripts/
+
+# Symlink the script
+rm /usr/local/bin/bsd
+ln -s /usr/local/bsd-scripts/bsd.sh /usr/local/bin/bsd
+echo
+echo 'Install complete!'
+
+rm -rf $BINTEMP
