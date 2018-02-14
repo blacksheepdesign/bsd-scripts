449a25943a27e2ac6a24f8b53b0df5a0f1f5a2cd
Jamie Morgan-Ward <jamie@blacksheepdesign.co.nz>
2018-02-15 10:06:47 +1300

    Fixed issue where scripts would install to the incorrect directory

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cd $BINTEMP
 curl -sL https://github.com/blacksheepdesign/bsd-scripts/archive/master.tar.gz | tar xz
 
 # Copy the scripts to /usr/local
-sudo rsync -a $BINTEMP/ /usr/local/bsd-scripts/
+sudo rsync -a $BINTEMP/bsd-scripts-master/ /usr/local/bsd-scripts/
 
 # Symlink the script
 rm -f /usr/local/bin/bsd
